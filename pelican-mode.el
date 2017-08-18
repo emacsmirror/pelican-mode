9bc77986bbc61389013030294e2036b8d8bb1ed3
Joe Wreschnig <joe.wreschnig@gmail.com>
2017-08-18 22:52:36 +0200

    Provide an example `use-package’ declaration.

--- pelican-mode.el
+++ pelican-mode.el
@@ -48,6 +48,13 @@
 ;;     (require 'pelican-mode)
 ;;     (pelican-global-mode)
 ;;
+;; Or with ``use-package'' and deferred loading:
+;;
+;;     (use-package pelican-mode
+;;       :after (:any org rst markdown-mode adoc-mode)
+;;       :config
+;;       (pelican-global-mode))
+;;
 ;; Or, register `pelican-mode' or `pelican-mode-enable-if-site'
 ;; as hook functions for more direct control.
 
